f0491bf66d05e60d2e6cf2bc9f159accda05db6c
williamfisher9 <fisher.a.william@gmail.com>
2024-07-27 23:29:00 -0500

    enhanced main menu css usage

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -2,9 +2,9 @@ import { useContext } from "react";
 import "./MainMenu.css";
 import { ThemeContext } from "../../App";
 
-import LogoIcon from "../LogoIcon";
+import LogoIcon from "../Logo/LogoIcon";
 
-import LogoTitle from '../LogoTitle'
+import LogoTitle from '../Logo/LogoTitle'
 
 function MainMenu() {
   const {theme} = useContext(ThemeContext);
@@ -21,67 +21,67 @@ function MainMenu() {
       </div>
 
       <div className="submenus">
-        <div className="top-submenu">
+        <div className="submenu">
           <div className="icon-container">
             <div className="inner-icon-container">
-              <span className={theme.type === 'dark' ? 'icon icon-dark material-symbols-outlined' : 'icon material-symbols-outlined icon-light'}>home</span>
+              <span className={theme.type === 'dark' ? 'main-menu-icon icon-dark material-symbols-outlined' : 'main-menu-icon material-symbols-outlined icon-light'}>home</span>
               <span className={theme.type === 'dark' ? 'icon-desc icon-desc-dark' : 'icon-desc icon-desc-light'}>home</span>
             </div>
           </div>
 
           <div className="icon-container">
             <div className="inner-icon-container">
-            <span className={theme.type === 'dark' ? 'icon icon-dark material-symbols-outlined' : 'icon material-symbols-outlined icon-light'}>apps</span>
+            <span className={theme.type === 'dark' ? 'main-menu-icon icon-dark material-symbols-outlined' : 'main-menu-icon material-symbols-outlined icon-light'}>apps</span>
             <span className={theme.type === 'dark' ? 'icon-desc icon-desc-dark' : 'icon-desc icon-desc-light'}>apps</span>
             </div>
           </div>
 
           <div className="icon-container">
             <div className="inner-icon-container">
-            <span className={theme.type === 'dark' ? 'icon icon-dark material-symbols-outlined' : 'icon material-symbols-outlined icon-light'}>token</span>
+            <span className={theme.type === 'dark' ? 'main-menu-icon icon-dark material-symbols-outlined' : 'main-menu-icon material-symbols-outlined icon-light'}>token</span>
             <span className={theme.type === 'dark' ? 'icon-desc icon-desc-dark' : 'icon-desc icon-desc-light'}>token</span>
             </div>
           </div>
 
           <div className="icon-container">
             <div className="inner-icon-container">
-            <span className={theme.type === 'dark' ? 'icon icon-dark material-symbols-outlined' : 'icon material-symbols-outlined icon-light'}>extension</span>
+            <span className={theme.type === 'dark' ? 'main-menu-icon icon-dark material-symbols-outlined' : 'main-menu-icon material-symbols-outlined icon-light'}>extension</span>
             <span className={theme.type === 'dark' ? 'icon-desc icon-desc-dark' : 'icon-desc icon-desc-light'}>plugins</span>
             </div>
           </div>
 
           <div className="icon-container">
             <div className="inner-icon-container">
-            <span className={theme.type === 'dark' ? 'icon icon-dark material-symbols-outlined' : 'icon material-symbols-outlined icon-light'}>group</span>
+            <span className={theme.type === 'dark' ? 'main-menu-icon icon-dark material-symbols-outlined' : 'main-menu-icon material-symbols-outlined icon-light'}>group</span>
             <span className={theme.type === 'dark' ? 'icon-desc icon-desc-dark' : 'icon-desc icon-desc-light'}>groups</span>
             </div>
           </div>
 
           <div className="icon-container">
             <div className="inner-icon-container">
-            <span className={theme.type === 'dark' ? 'icon icon-dark material-symbols-outlined' : 'icon material-symbols-outlined icon-light'}>cookie</span>
+            <span className={theme.type === 'dark' ? 'main-menu-icon icon-dark material-symbols-outlined' : 'main-menu-icon material-symbols-outlined icon-light'}>cookie</span>
             <span className={theme.type === 'dark' ? 'icon-desc icon-desc-dark' : 'icon-desc icon-desc-light'}>cookies</span>
             </div>
           </div>
 
           <div className="icon-container">
             <div className="inner-icon-container">
-            <span className={theme.type === 'dark' ? 'icon icon-dark material-symbols-outlined' : 'icon material-symbols-outlined icon-light'}>new_releases</span>
+            <span className={theme.type === 'dark' ? 'main-menu-icon icon-dark material-symbols-outlined' : 'main-menu-icon material-symbols-outlined icon-light'}>new_releases</span>
             <span className={theme.type === 'dark' ? 'icon-desc icon-desc-dark' : 'icon-desc icon-desc-light'}>releases</span>
             </div>
           </div>
         </div>
 
-        <div className="bottom-submenu">
+        <div className="submenu">
           <div className="icon-container">
             <div className="inner-icon-container">
-            <span className={theme.type === 'dark' ? 'icon icon-dark material-symbols-outlined' : 'icon material-symbols-outlined icon-light'}>settings</span>
+            <span className={theme.type === 'dark' ? 'main-menu-icon icon-dark material-symbols-outlined' : 'main-menu-icon material-symbols-outlined icon-light'}>settings</span>
             <span className={theme.type === 'dark' ? 'icon-desc icon-desc-dark' : 'icon-desc icon-desc-light'}>settings</span>
             </div>
           </div>
           <div className="icon-container">
             <div className="inner-icon-container">
-            <span className={theme.type === 'dark' ? 'icon icon-dark material-symbols-outlined' : 'icon material-symbols-outlined icon-light'}>logout</span>
+            <span className={theme.type === 'dark' ? 'main-menu-icon icon-dark material-symbols-outlined' : 'main-menu-icon material-symbols-outlined icon-light'}>logout</span>
             <span className={theme.type === 'dark' ? 'icon-desc icon-desc-dark' : 'icon-desc icon-desc-light'}>logout</span>
             </div>
           </div>
--- src/components/SocialMenu/SocialMenu.jsx
+++ src/components/SocialMenu/SocialMenu.jsx
@@ -1,26 +1,65 @@
-import { useContext } from 'react';
-import './SocialMenu.css'
-import { ThemeContext } from '../../App';
-import {lightStyle, darkStyle} from '../../ConstValues'
+import { useContext } from "react";
+import "./SocialMenu.css";
+import { ThemeContext } from "../../App";
 
 function SocialMenu() {
+  const { theme, setTheme } = useContext(ThemeContext);
 
-    const {theme, setTheme} = useContext(ThemeContext);
+  const handleThemeChanger = () => {
+    console.log(`current theme: ${theme.type}`);
+    theme.type === "dark"
+      ? setTheme({ ...theme, type: "light" })
+      : setTheme({ ...theme, type: "dark" });
+  };
 
-    const handleThemeChanger = () => {
-        console.log(`current theme: ${theme.type}`);
-        theme.type === 'dark' ? setTheme({...theme, type: 'light'}) : setTheme({...theme, type: 'dark'});
-    }
-
-    return <div className='social-menu-container' style={{backgroundColor:  theme.type === 'dark' ? theme.dark.menuColor : theme.light.menuColor}}>
-        <div className='social-menu'>
-        <i className={theme.type === 'dark' ? "icon icon-dark fa-regular fa-sun" : "icon icon-light fa-regular fa-moon"} onClick={handleThemeChanger} ></i>
-        <i className={theme.type === 'dark' ? "icon icon-dark fa-brands fa-square-facebook" : "icon icon-light fa-brands fa-square-facebook"}></i>
-        <i className={theme.type === 'dark' ? "icon icon-dark fa-brands fa-x-twitter" : "icon icon-light fa-brands fa-x-twitter"}></i>
-        <i className={theme.type === 'dark' ? "icon icon-dark fa-brands fa-instagram" : "icon icon-light fa-brands fa-instagram"}></i>
-        </div>
+  return (
+    <div
+      className="social-menu-container"
+      style={{
+        backgroundColor:
+          theme.type === "dark" ? theme.dark.menuColor : theme.light.menuColor,
+      }}
+    >
+      <div className="social-menu">
+        <i
+          className={
+            theme.type === "dark"
+              ? "icon icon-dark fa-regular fa-sun"
+              : "icon icon-light fa-regular fa-moon"
+          }
+          onClick={handleThemeChanger}
+        ></i>
+        <hr
+          style={{
+            width: "30px",
+            height: "4px",
+            backgroundColor: theme.type === "dark" ? "#fff" : "#000",
+          }}
+        />
+        <i
+          className={
+            theme.type === "dark"
+              ? "icon icon-dark fa-brands fa-square-facebook"
+              : "icon icon-light fa-brands fa-square-facebook"
+          }
+        ></i>
+        <i
+          className={
+            theme.type === "dark"
+              ? "icon icon-dark fa-brands fa-x-twitter"
+              : "icon icon-light fa-brands fa-x-twitter"
+          }
+        ></i>
+        <i
+          className={
+            theme.type === "dark"
+              ? "icon icon-dark fa-brands fa-instagram"
+              : "icon icon-light fa-brands fa-instagram"
+          }
+        ></i>
+      </div>
     </div>
-
+  );
 }
 
 export default SocialMenu;
